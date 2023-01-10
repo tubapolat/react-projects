0ca6f832dd0f7aef95d727fbe2ddbd02a80769d5
tubapolat <tubakalay@gmail.com>
2023-01-10 22:47:48 +0000

    finalising

--- e-commerce/client/src/pages/Auth/Signin/validations.js
+++ e-commerce/client/src/pages/Auth/Signin/validations.js
@@ -3,11 +3,11 @@ import * as yup from "yup";
 const validations = yup.object().shape({
 	email: yup
 		.string()
-		.email("Gerçerli bir email girin.")
-		.required("Zorunlu alan."),
+		.email("Please enter a valid e-mail address.")
+		.required("Required field."),
 	password: yup
 		.string()
-		.min(5, "Parolanız en az 5 karakter olmalıdır")
+		.min(5, "Your password should be at least 4 characters")
 		.required(),
 });
 
