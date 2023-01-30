f4cf86e2a9aade0bdc49f7613c23848023c9aa17
tubapolat <tubakalay@gmail.com>
2023-01-30 22:47:34 +0000

    finalising

--- to/src/App.js
+++ to/src/App.js
@@ -1,34 +1,63 @@
 import React, { useState } from "react";
+import AddTaskForm from './components/AddTaskForm';
+import UpdateForm from './components/UpdateForm';
+import ToDo from './components/ToDo';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCircleCheck,
-  faPen,
-  faTasks,
-  faTrashCan,
-} from "@fortawesome/free-solid-svg-icons";
+
 import "./App.css";
 
 function App() {
   const [toDo, setToDo] = useState([
-    { id: 1, title: "task 1", satus: false },
-    { id: 2, title: "task 2", satus: false },
+    { id: 1, title: "task 1", status: false },
+    { id: 2, title: "task 2", status: false },
   ]);
 
-  const [newTask, setNewtask] = useState("");
+  const [newTask, setNewTask] = useState("");
   const [updateData, setUpdateData] = useState("");
 
-  const AddTask = () => {};
+  const addTask = () => {
+    if (newTask) {
+      let num = toDo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false };
+      setToDo([...toDo, newEntry]);
+      setNewTask("");
+    }
+  };
 
-  const deleteTask = () => {};
+  const deleteTask = (id) => {
+    let newTasks = toDo.filter((task) => task.id !== id);
+    setToDo(newTasks);
+  };
 
-  const markDone = (id) => {};
+  const markDone = (id) => {
+    let newTasks = toDo.map((task) => {
+      if (task.id === id) {
+        return { ...task, status: !task.status };
+      }
+      return task;
+    });
+    setToDo(newTasks);
+  };
 
-  const cancelUpdate = () => {};
+  const cancelUpdate = () => {
+    setUpdateData('');
+  };
 
-  const changeTask = (e) => {};
+  const changeTask = (e) => {
+    let newEntry= {
+      id: updateData.id,
+      title: e.target.value,
+      status: updateData.status ? true : false
+    }
+    setUpdateData(newEntry);
+  };
 
-  const updateTask = () => {};
+  const updateTask = () => {
+    let filterRecords = [...toDo].filter ( task => task.id !== updateData.id);
+    let updatedObject = [...filterRecords, updateData]
+    setToDo(updatedObject);
+    setUpdateData('');
+  };
 
   return (
     <div className="container App">
@@ -37,26 +66,33 @@ function App() {
       <h2>To Do List App</h2>
       <br></br>
 
-      {toDo && toDo.length ? "" : "No tasks...!"}
+      {updateData && updateData ? (
+       <UpdateForm
+       updateData= {updateData}
+        changeTask={changeTask}
+        updateTask={updateTask}
+        cancelUpdate={cancelUpdate}
+       />
+      ) : (
+        
+      <AddTaskForm
+      newTask={newTask}
+      setNewTask={setNewTask}
+      addTask={addTask}
+      />
+      )}
+      
+      <br />
+  
 
-      {toDo &&
-        toDo.map((task, index) => {
-          return (
-            <React.Fragment key={task.id}>
-              <div className="col taskBg">
-                <div className={task.status ? "done" : ""}>
-                  <span className="taskNumber">{index + 1}</span>
-                  <span className="taskText">{task.title}</span>
-                </div>
-                <div className="iconsWrap">
-                  <span> <FontAwesomeIcon icon={faCircleCheck}/></span>
-                  <span> <FontAwesomeIcon icon={faPen}/> </span>
-                  <span> <FontAwesomeIcon icon={faTrashCan}/></span>
-                </div>
-              </div>
-            </React.Fragment>
-          );
-        })}
+      {toDo && toDo.length ? "" : "No tasks...!"}
+<ToDo
+toDo={toDo}
+markDone={markDone}
+setUpdateData={setUpdateData}
+deleteTask={deleteTask}
+/>
+      
     </div>
   );
 }
---
+++ to/src/components/AddTaskForm.jsx
@@ -0,0 +1,20 @@
+const AddTaskForm = ({ newTask,setNewTask, addTask }) =>{
+    return(
+<div className="row">
+        <div className="col">
+          <input
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            className="form-control form-control-lg"
+          ></input>
+        </div>
+        <div className="col-auto">
+          <button onClick={addTask} className="btn btn-lg btn-success mr-20">
+            Add task
+          </button>
+        </div>
+      </div>
+    )
+}
+
+export default AddTaskForm;
