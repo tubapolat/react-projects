ab5145f2b5b11cbf26e8ed51d2510a901c933661
tubapolat <tubakalay@gmail.com>
2023-03-12 13:55:52 +0000

    adding css

--- memory-art/src/App.js
+++ memory-art/src/App.js
@@ -1,26 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { src: "/img/angel.jpg" },
-  { src: "/img/moulin.jpg" },
-  { src: "/img/children.jpg" },
-  { src: "/img/arbore.jpg" },
-  { src: "/img/cassatt.jpg" },
-  { src: "/img/forest.jpg" },
-  { src: "/img/gauguin.jpg" },
-  { src: "/img/gwen.jpg" },
-  { src: "/img/kisling.jpg" },
-  { src: "/img/maurice.jpg" },
-  { src: "/img/monet.jpg" },
-  { src: "/img/morrisot.jpg" },
-  { src: "/img/players.jpg" },
-  { src: "/img/starry.jpg" },
-  { src: "/img/sun.jpg" },
-  { src: "/img/sunflowers.jpg" },
-  { src: "/img/suzanne.jpg" },
-  { src: "/img/van-gogh.jpg" },
+  { src: "/img/angel.jpg", matched: false },
+  { src: "/img/moulin.jpg", matched: false },
+  { src: "/img/children.jpg", matched: false },
+  { src: "/img/arbore.jpg", matched: false },
+  { src: "/img/cassatt.jpg", matched: false },
+  { src: "/img/forest.jpg", matched: false },
+  { src: "/img/gauguin.jpg", matched: false },
+  { src: "/img/gwen.jpg", matched: false },
+  { src: "/img/kisling.jpg", matched: false },
+  { src: "/img/maurice.jpg", matched: false },
+  { src: "/img/monet.jpg", matched: false },
+  { src: "/img/morrisot.jpg", matched: false },
+  { src: "/img/players.jpg", matched: false },
+  { src: "/img/starry.jpg", matched: false },
+  { src: "/img/sun.jpg", matched: false },
+  { src: "/img/sunflowers.jpg", matched: false },
+  { src: "/img/suzanne.jpg", matched: false },
+  { src: "/img/van-gogh.jpg", matched: false },
 ];
 
 function App() {
@@ -28,12 +28,15 @@ function App() {
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
 
+    setChoiceOne(null);
+    setChoiceTwo(null);
     setCards(shuffledCards);
     setTurns(0);
   };
@@ -42,11 +45,39 @@ function App() {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
 
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+
   const resetTurn = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
     setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
   };
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
 
   return (
     <div className="App">
@@ -55,9 +86,16 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard card={card} key={card.id} handleChoice={handleChoice} />
+          <SingleCard
+            card={card}
+            key={card.id}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
         ))}
       </div>
+      <p> Turns: {turns}</p>
     </div>
   );
 }
--- memory-art/src/components/SingleCard/index.jsx
+++ memory-art/src/components/SingleCard/index.jsx
@@ -1,13 +1,15 @@
 import "./singleCard.css";
 
-export default function SingleCard({ card, handleChoice }) {
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
   const handleClick = () => {
-    handleChoice(card);
+    if (!disabled) {
+      handleChoice(card);
+    }
   };
 
   return (
     <div className="card">
-      <div>
+      <div className={flipped ? "flipped" : ""}>
         <img className="front" src={card.src} alt="card-front" />
         <img
           className="back"
