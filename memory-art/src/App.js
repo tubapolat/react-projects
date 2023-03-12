b069d672f28aa53289de570a317cb67926e04b69
tubapolat <tubakalay@gmail.com>
2023-03-12 21:26:19 +0000

    finalising

--- memory-art/src/App.js
+++ memory-art/src/App.js
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import SingleCard from "./components/SingleCard";
+import { v4 as uuidv4 } from "uuid";
 
 const cardImages = [
   { src: "/img/angel.jpg", matched: false },
-  { src: "/img/moulin.jpg", matched: false },
+  //{ src: "/img/moulin.jpg", matched: false },
   { src: "/img/children.jpg", matched: false },
-  { src: "/img/arbore.jpg", matched: false },
+  // { src: "/img/arbore.jpg", matched: false },
   { src: "/img/cassatt.jpg", matched: false },
-  { src: "/img/forest.jpg", matched: false },
-  { src: "/img/gauguin.jpg", matched: false },
+  //{ src: "/img/forest.jpg", matched: false },
+  // { src: "/img/gauguin.jpg", matched: false },
   { src: "/img/gwen.jpg", matched: false },
-  { src: "/img/kisling.jpg", matched: false },
+  // { src: "/img/kisling.jpg", matched: false },
   { src: "/img/maurice.jpg", matched: false },
   { src: "/img/monet.jpg", matched: false },
   { src: "/img/morrisot.jpg", matched: false },
-  { src: "/img/players.jpg", matched: false },
+  //{ src: "/img/players.jpg", matched: false },
   { src: "/img/starry.jpg", matched: false },
   { src: "/img/sun.jpg", matched: false },
   { src: "/img/sunflowers.jpg", matched: false },
@@ -29,11 +30,12 @@ function App() {
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
   const [disabled, setDisabled] = useState(false);
+  const [score, setScore] = useState(0);
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }));
+      .map((card) => ({ ...card, id: uuidv4() }));
 
     setChoiceOne(null);
     setChoiceTwo(null);
@@ -42,7 +44,11 @@ function App() {
   };
 
   const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+    if (choiceOne && choiceOne.id !== card.id) {
+      setChoiceTwo(card);
+    } else {
+      setChoiceOne(card);
+    }
   };
 
   useEffect(() => {
@@ -66,8 +72,6 @@ function App() {
     }
   }, [choiceOne, choiceTwo]);
 
-  console.log(cards);
-
   const resetTurn = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
@@ -79,9 +83,18 @@ function App() {
     shuffleCards();
   }, []);
 
+  useEffect(() => {
+    // const isGameOver =  cards.length !== 0 && cards.every((card) => card.matched === true);
+    // if (isGameOver) {
+    const userScore = 1200 - 15 * (turns * 2 - 24);
+    if (userScore > 0) setScore(userScore);
+    // }
+  }, [cards, turns]);
+
   return (
     <div className="App">
       <h1>Memory Art</h1>
+      <h5>Your Score: {score}</h5>
       <button onClick={shuffleCards}>New Game</button>
 
       <div className="card-grid">
