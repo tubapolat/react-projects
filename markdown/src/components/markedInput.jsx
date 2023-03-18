d28bd44a9b9385e50384cb063683842e89c21c71
tubapolat <tubakalay@gmail.com>
2023-03-18 15:05:48 +0000

    initial commit

---
+++ markdown/src/components/markedInput.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+import editorContext from "../editorContext";
+
+const Container = styled.div`
+  width: 50%;
+  height: 100%;
+  padding: 13px;
+  border-right: 1.5px solid rgba(15, 15, 15, 0.4);
+  font-family: "lato", sans-serif;
+`;
+
+const Title = styled.div`
+  font-size: 22px;
+  font-weight: 600;
+  margin-bottom: 1em;
+  padding: 8px 0;
+  border-bottom: 1px solid rgba(15, 15, 15, 0.3);
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 100%;
+  resize: none;
+  border: none;
+  outline: none;
+  font-size: 17px;
+`;
+
+export function MarkedInput(props) {
+  const { setMarkeddownText } = useContext(editorContext);
+
+  const onInputChange = (e) => {
+    const newvalue = e.currentTarget.value;
+    setMarkeddownText(newvalue);
+  };
+
+  return (
+    <Container>
+      <Title>Markdown Text</Title>
+      <TextArea onChange={onInputChange} />
+    </Container>
+  );
+}
